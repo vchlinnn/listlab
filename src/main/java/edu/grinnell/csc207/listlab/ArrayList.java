48f0aef8c815562f27403c2e151438bacfa25c87
Vu <vulinh@hermann.cs.grinnell.edu>
2025-02-19 14:21:38 -0600

    Finish basic implementation

--- src/main/java/edu/grinnell/csc207/listlab/ArrayList.java
+++ src/main/java/edu/grinnell/csc207/listlab/ArrayList.java
@@ -1,23 +1,45 @@
 package edu.grinnell.csc207.listlab;
 
+import java.util.Arrays;
+
 /**
  * An array-based implementation of the list ADT.
  */
 public class ArrayList {
+    
+    private int[] array;
+    private int size; // the number of elements 
+    private static final int INITIAL_SIZE = 50;
+    
+    // the length of the backing array: capacity
+    
+    public ArrayList() {
+        this.array = new int[INITIAL_SIZE];
+        this.size = 0;
+    }
+    
+    private void ensureCapacity() {
+        if(size == array.length){
+            // allocate a new, bigger array
+            array = Arrays.copyOf(array, array.length * 2);
+        }
+    }
     /**
      * Adds <code>value</code> to the end of the list
      * 
      * @param value the value to add to the end of the list
      */
     public void add(int value) {
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+        ensureCapacity();
+        array[size] = value;
+        size++;
     }
 
     /**
      * @return the number of elements in the list
      */
     public int size() {
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
+        return size;
     }
 
     /**
@@ -25,7 +47,7 @@ public class ArrayList {
      * @return the value at the specified <code>index</code>
      */
     public int get(int index) {
-        throw new UnsupportedOperationException("Unimplemented method 'get'");
+        return array[index];
     }
 
     /**
@@ -35,6 +57,10 @@ public class ArrayList {
      * @return the element at <code>index</code>
      */
     public int remove(int index) {
-        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+        int removed = array[index];
+        for (int i = index; i < size; i++) {
+            array[i] = array[i + 1];
+        }
+        return removed;
     }
 }
--- src/main/java/edu/grinnell/csc207/listlab/LinkedList.java
+++ src/main/java/edu/grinnell/csc207/listlab/LinkedList.java
@@ -4,20 +4,53 @@ package edu.grinnell.csc207.listlab;
  * A linked implementation of the list ADT.
  */
 public class LinkedList {
+
+    private static class Node {
+
+        public int value;
+        public Node next;
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private Node first;
+
+    public LinkedList() {
+        this.first = null;
+    }
+
     /**
      * Adds <code>value</code> to the end of the list
-     * 
+     *
      * @param value the value to add to the end of the list
      */
     public void add(int value) {
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+        if (first == null) {
+            first = new Node(value, null);
+        } else {
+            Node cur = first;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            Node last_node = new Node(value, null);
+            cur.next = last_node;
+        }
     }
 
     /**
      * @return the number of elements in the list
      */
     public int size() {
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
+        Node temp = first;
+        int count = 0;
+        while (temp.next != null) {
+            temp = temp.next;
+            count++;
+        }
+        return count;
     }
 
     /**
@@ -25,16 +58,36 @@ public class LinkedList {
      * @return the value at the specified <code>index</code>
      */
     public int get(int index) {
-        throw new UnsupportedOperationException("Unimplemented method 'get'");
+        Node temp = first;
+        int count = 0;
+        while (temp.next != null && count != index) {
+            temp = temp.next;
+            count++;
+        }
+        return temp.value;
     }
 
     /**
      * Removes the value at <code>index</code> from the list
-     * 
+     *
      * @param index the index of the element to remove
      * @return the element at <code>index</code>
+     * @throws java.lang.Exception
      */
-    public int remove(int index) {
-        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    public int remove(int index) throws Exception {
+    
+        if (first == null) {
+            throw new Exception();
+        }
+        Node temp = first;
+        int count = 0;
+        while (temp.next != null && count != index - 1) {
+            temp = temp.next;
+            count++;
+        }
+        int removed = temp.next.value;
+        temp.next = temp.next.next;
+        return removed;
+
     }
 }
